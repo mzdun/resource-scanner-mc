241b6cc204eea7bd1375ea126a35ad992fd5ee54
Marcin Zdun <marcin.zdun@gmail.com>
2024-09-17 15:04:58 +0200

    build: allow adding new fabric targets (#6)

---
+++ tools/add-fabric.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shlex
+import subprocess
+import xml.etree.ElementTree as ET
+
+from cache import Cache
+
+__root__ = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+cache = Cache(os.path.join(__root__, "tools", ".fabric-cache"))
+
+
+def fabric(path: str):
+    return cache.json(f'https://meta.fabricmc.net/v2/versions/{path}')
+
+
+def getFirst(flt, data):
+    for item in filter(flt, data):
+        return item
+    return None
+
+
+def getFirstStable(data):
+    return getFirst(
+        lambda item: item.get("stable") == True,
+        data
+    )
+
+
+def gameVersionForApi(version: str) -> str:
+    if version == "1.18":
+        return "0.44.0+1.18"
+    gameVersion = version.split('-', 1)[0]
+
+    for ver in [
+        ["22w13oneblockatatime"],
+        ["24w", "1.21.2"],
+        ["23w", "1.20.5"],
+        ["22w", "1.19.3"],
+        ["21w", "1.18"],
+        ["20w", "1.17"],
+        ["19w", "1.14"],
+        ["18w", "1.14"]
+    ]:
+        prefix, branchName = ver if len(ver) == 2 else [ver[0], ver[0]]
+        if gameVersion.startswith(prefix):
+            return branchName
+
+    return gameVersion.split('_', 1)[0]
+
+
+def any(ign): return True
+
+
+def prerelease(api: str):
+    curr = [int(v) for v in api.split('.')]
+    prev = [v for v in curr]
+    index = len(prev) - 1
+    while index > -1:
+        if prev[index] > 0:
+            prev[index] -= 1
+            break
+        index -= 1
+
+    prevApi = '.'.join(str(v) for v in prev)
+    return f'>{prevApi}'
+
+
+def __main__():
+    version = sys.argv[1]
+
+    game = fabric('game')
+    game = getFirstStable(game) if version == '--stable' \
+        else getFirst(any, game) if version == '--latest' \
+        else getFirst(
+            lambda item: item.get('version') == version,
+            game
+    )
+
+    if game is None or 'version' not in game:
+        print(f'cannot find Minecraft version for {version}')
+        exit(1)
+
+    version = game['version']
+    apiVersion = gameVersionForApi(version)
+
+    def validApiVersion(api: str) -> bool:
+        return api.endswith(f'-{apiVersion}') or api.endswith(f'+{apiVersion}')
+
+    allGameYarn = list(filter(
+        lambda item: item.get('gameVersion') == version,
+        fabric('yarn')
+    ))
+
+    stableYarn = getFirstStable(allGameYarn)
+    firstYarn = getFirst(any, allGameYarn)
+    yarn = firstYarn if stableYarn is None or 'version' not in stableYarn else stableYarn
+    if yarn is None or 'version' not in yarn:
+        print(f'cannot find Yarn for Minecraft version {version}')
+        exit(1)
+
+    loader = getFirstStable(fabric('loader'))
+    if loader is None or 'version' not in loader:
+        print(f'cannot find a stable loader')
+        exit(1)
+
+    maven = cache.load(
+        'https://maven.fabricmc.net/net/fabricmc/fabric-api/fabric-api/maven-metadata.xml')
+    maven = ET.fromstring(maven).iter('version')
+    maven = map(lambda e: e.text, maven)
+    maven = filter(validApiVersion, maven)
+    maven = list(maven)
+    fabricAPI = maven[-1] if len(maven) else None
+    if fabricAPI is None:
+        print(f'cannot find Fabric API for Minecraft version {version}')
+        exit(1)
+
+    minecraft_dependency = ''
+    if version != apiVersion:
+        minecraft_dependency = f'\nminecraft_dependency={
+            prerelease(apiVersion)}'
+
+    template = f"""# Fabric Properties
+# check these on https://fabricmc.net/develop
+minecraft_version={game['version']}{minecraft_dependency}
+yarn_mappings={yarn['version']}
+loader_version={loader['version']}
+
+# Fabric API
+fabric_version={fabricAPI}
+
+# Mod Properties
+mod_version=${{mod_version}}-${{minecraft_version}}-fabric
+"""
+    print(template)
+
+    dirname = os.path.join(__root__, 'fabric', version)
+    if os.path.isdir(dirname):
+        print(f'Directory {dirname} already exists')
+        return
+
+    os.makedirs(dirname, exist_ok=True)
+    os.chdir(dirname)
+    os.symlink(os.path.join('..', 'any', 'src'),
+               'src', target_is_directory=True)
+    os.symlink(os.path.join(
+        '..', 'any', 'build.gradle'), 'build.gradle')
+    with open('gradle.properties', 'w', encoding='UTF-8') as properties:
+        print(template, file=properties)
+
+    with open(os.path.join(__root__, 'settings.gradle'), 'a', encoding='UTF-8') as settings:
+        print(f'include "fabric:{version}"', file=settings)
+
+    print('Now visit https://fabricmc.net/develop/')
+
+    propsArg = shlex.quote(os.path.join(dirname, 'gradle.properties'))
+    if os.environ.get('TERM_PROGRAM') == 'vscode':
+        print(f'code {propsArg}')
+        subprocess.run(['code', os.path.join(
+            dirname, 'gradle.properties')], shell=True)
+
+
+if __name__ == '__main__':
+    sys.exit(__main__())
---
+++ tools/cache.py
@@ -0,0 +1,182 @@
+import email
+import http.client
+import json
+import os
+import time
+import uuid
+
+from dataclasses import dataclass
+from typing import Dict, List, Optional, Union
+from urllib.parse import urlparse, ParseResult
+
+
+@dataclass
+class CacheItem:
+    uri: str
+    path: str
+    charset: Optional[str]
+    modified: Optional[str]
+    expires: Optional[float]
+
+    def get_charset(self):
+        return self.charset if self.charset is not None else 'UTF-8'
+
+    def load(self, dirName: str, binary: bool = False):
+        try:
+            with open(os.path.join(dirName, "items", self.path), "rb") as input:
+                data: bytes = input.read()
+            return data if binary else data.decode(self.get_charset())
+        except BaseException:
+            return None
+
+    def dump(self):
+        result = {
+            "uri": self.uri,
+            "path": self.path,
+        }
+        if self.charset is not None:
+            result["charset"] = self.charset
+        if self.modified is not None:
+            result["modified"] = self.modified
+        if self.expires is not None:
+            result["expires"] = self.expires
+        return result
+
+    def expired(self):
+        if self.expires is None:
+            return False
+        return self.expires < time.time()
+
+
+class Cache:
+    dirName: str
+    items: List[CacheItem] = []
+    refs: Dict[str, CacheItem] = {}
+    conn: Optional[http.client.HTTPSConnection] = None
+    netloc: str = ''
+
+    def __init__(self, dirName: str):
+        self.dirName = dirName
+        self.init()
+
+    def init(self):
+        index = os.path.join(self.dirName, 'index.json')
+        try:
+            with open(index) as indexData:
+                data: List[Dict] = json.load(indexData)
+            for item in data:
+                uri = item.get("uri")
+                path = item.get("path")
+                charset = item.get("charset")
+                modified = item.get("modified")
+                expires = item.get("expires")
+                if uri is None or path is None:
+                    continue
+
+                item = CacheItem(uri, path, charset, modified, expires)
+                prev = self.refs.get(item.uri)
+                if prev:
+                    self.items.remove(prev)
+                self.refs[item.uri] = item
+                self.items.append(item)
+            self.items.sort(key=lambda item: item.uri)
+        except BaseException:
+            self.items = []
+            self.refs = {}
+
+    def load(self, uri: str, binary: bool = False):
+        url = urlparse(uri)
+        item = self.refs.get(url.geturl())
+        if item is None:
+            return self._load(url, binary)
+        if item.expired():
+            return self._load(url, binary, item)
+        if item.modified is not None:
+            return self._load(url, binary, item, {'If-Modified-Since': item.modified})
+        return item.load(self.dirName, binary)
+
+    def json(self, uri: str):
+        data = self.load(uri)
+        if data is None:
+            return None
+        return json.loads(data)
+
+    def _load(self, url: ParseResult, binary: bool, prevItem: Optional[CacheItem] = None, headers: Dict[str, str] = {}) -> Union[str, bytes]:
+        resource = url.path
+        if url.query != '':
+            resource = f'{resource}?{url.query}'
+
+        if self.conn is None or url.netloc != self.netloc:
+            self.conn = http.client.HTTPSConnection(url.netloc)
+            self.netloc = url.netloc
+        self.conn.request('GET', resource, headers=headers)
+        response = self.conn.getresponse()
+
+        # print(response.status, response.reason, url.geturl())
+        if response.status > 199 and response.status < 300:
+            return self._load2xx(url, response, binary, prevItem)
+
+        response.read()
+
+        if response.status < 400 and prevItem is not None:
+            return prevItem.load(self.dirName, binary)
+
+        return None
+
+    def _load2xx(self, url: ParseResult, response: http.client.HTTPResponse, binary: bool, prevItem: Optional[CacheItem] = None):
+        data = response.read()
+
+        http_modified = response.headers.get('Last-Modified')
+        http_expires = response.headers.get('Expires')
+
+        expires: Optional[float] = None
+        if http_expires is not None:
+            dateTuple = email.utils.parsedate(http_expires.strip())
+            if dateTuple is not None:
+                expires = time.mktime(dateTuple)
+
+        http_charset = response.headers.get_charset()
+        if http_charset is None:
+            content_type = response.headers.get(
+                'Content-Type', '').split(';')[1:]
+            content_type = [chunk.strip().split('=', 1)
+                            for chunk in content_type]
+            content_type = {key.strip().lower(): value.strip()
+                            for key, value in content_type}
+            http_charset = content_type.get('charset')
+
+        item = CacheItem(url.geturl(), prevItem.path if prevItem is not None else self._randomPath(), charset=http_charset,
+                         modified=http_modified, expires=expires)
+        self._store(item, data)
+
+        return item.load(self.dirName, binary)
+
+    def _store(self, item: CacheItem, data: bytes):
+        useItem = item.expires is not None or item.modified is not None
+        if not useItem:
+            return
+
+        prev = self.refs.get(item.uri)
+        if prev:
+            self.items.remove(prev)
+        self.refs[item.uri] = item
+        self.items.append(item)
+        self.items.sort(key=lambda item: item.uri)
+
+        os.makedirs(os.path.dirname(os.path.join(
+            self.dirName, 'items', item.path)), exist_ok=True)
+
+        with open(os.path.join(self.dirName, 'items', item.path), "wb") as output:
+            output.write(data)
+
+        with open(os.path.join(self.dirName, "index.json"), "w", encoding="UTF-8") as output:
+            items: List[dict] = [item.dump() for item in self.items]
+            json.dump(items, output, indent=2)
+
+    def _randomPath(self):
+        while True:
+            key = str(uuid.uuid4()).replace('-', '')
+            path = os.path.join(self.dirName, "items", key)
+            if os.path.exists(path):
+                continue
+            return key
