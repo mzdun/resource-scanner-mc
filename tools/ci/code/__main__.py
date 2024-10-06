b82daf8f4416c2651a2d727e8738aaf443e35bd7
Marcin Zdun <marcin.zdun@gmail.com>
2024-10-06 19:11:16 +0200

    chore: keep license hooks in

--- tools/ci/code/__main__.py
+++ tools/ci/code/__main__.py
@@ -9,10 +9,10 @@ import os
 import re
 import subprocess
 import sys
-from typing import Set
+from typing import List, Set
 
 from ..common.packages import safeRegex
-from ..common.runner import Environment
+from ..common.runner import Environment, capture
 from ..common.utils import PROJECT_ROOT, get_prog
 
 PACKAGE = "com.midnightbits.scanner"
@@ -39,14 +39,21 @@ license = commands.add_parser(
 
 Environment.addArgumentsTo(license)
 
+license.add_argument(
+    "--check",
+    action="store_true",
+    required=False,
+    help="verifies, if all Java files have the notification",
+)
 
-def licenses():
-    ROOT = os.path.join(PROJECT_ROOT, "")
-    PKG = os.path.join(*PACKAGE.split("."))
+license.add_argument(
+    "--check-staged",
+    action="store_true",
+    required=False,
+    help="verifies, if all staged Java files have the notification",
+)
 
-    FILENAME_REPLACEMENT = [(os.path.join("src", srcSet, "java", PKG),
-                             os.path.join(f"${srcSet.upper()}", "$PKG")) for srcSet in SRC_SETS]
-
+def build_regexp():
     maskChunks = MASK.split('{YEAR}')
     reMaskContentsLines = r'\d+'.join([safeRegex(chunk)
                                       for chunk in maskChunks]).split('\n')
@@ -56,7 +63,47 @@ def licenses():
     year = str(datetime.date.today().year)
     newHeading = year.join(maskChunks) + "\n\n"
 
-    # files: Set[str] = set()
+    return (matcher, newHeading)
+
+
+def matches(matcher: re.Pattern[str], text: List[str]):
+    head = '\n'.join([line.rstrip() for line in text[:2]])
+    return matcher.match(head) is not None
+
+
+def check_staged():
+    proc = capture('git','diff','--name-only','--cached')
+    filenames = proc.stdout.decode("UTF-8").split("\n")
+    matcher, _ = build_regexp()
+
+    files: Set[str] = set()
+    for path in filenames:
+        if path[-len(EXT):] != EXT:
+            continue
+        
+        proc = capture('git', 'show', f':{path}')
+        if not matches(matcher, proc.stdout.decode("UTF-8").split("\n")):
+            files.add(path)
+
+    if len(files) > 0:
+        print(f'Found {len(files)} file{"s" if len(files) != 1 else ""} without license notification:\n', file=sys.stderr)
+        for file in sorted(files):
+            print('-', file, file=sys.stderr)
+
+        print(f'\nTo fix {"them" if len(files) != 1 else "it"}, run\n\n    python -m tools.ci code license\n\nand stage them again.', file=sys.stderr)
+        exit(1)
+
+
+def licenses(check: bool):
+    ROOT = os.path.join(PROJECT_ROOT, "")
+    PKG = os.path.join(*PACKAGE.split("."))
+
+    FILENAME_REPLACEMENT = [(os.path.join("src", srcSet, "java", PKG),
+                             os.path.join(f"${srcSet.upper()}", "$PKG")) for srcSet in SRC_SETS]
+
+    matcher, newHeading = build_regexp()
+
+    files: Set[str] = set()
     for root, _, filenames in os.walk(PROJECT_ROOT):
         for filename in filenames:
             if filename[-len(EXT):] != EXT:
@@ -65,10 +112,12 @@ def licenses():
 
             with open(path, encoding="UTF-8") as inFile:
                 text = inFile.readlines()
-            head = '\n'.join([line.rstrip() for line in text[:2]])
-
-            m = matcher.match(head)
-            if m is not None:
+                
+            if matches(matcher, text):
+                continue
+            
+            if check:
+                files.add(path[len(ROOT):])
                 continue
 
             printPath = path[len(ROOT):]
@@ -80,13 +129,23 @@ def licenses():
             with open(path, "wb") as outFile:
                 outFile.write(newText.encode('UTF-8'))
 
+    if check and len(files) > 0:
+        print(f'Found {len(files)} file{"s" if len(files) != 1 else ""} without license notification:\n', file=sys.stderr)
+        for file in sorted(files):
+            print('-', file, file=sys.stderr)
+
+        print(f'\nTo fix {"them" if len(files) != 1 else "it"}, run\n\n    python -m tools.ci code license\n', file=sys.stderr)
+        exit(1)
 
 def __main__():
     args = parser.parse_args()
     Environment.apply(args)
 
     try:
-        licenses()
+        if args.check_staged:
+            check_staged()
+        else:
+            licenses(args.check)
     except subprocess.CalledProcessError as e:
         if e.stdout:
             print(e.stdout.decode("utf-8"), file=sys.stdout)
