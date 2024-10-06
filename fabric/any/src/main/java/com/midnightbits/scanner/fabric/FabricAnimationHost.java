4e37c22e81378fe5da71ed0b755e34046e1afc67
Marcin Zdun <marcin.zdun@gmail.com>
2024-10-06 08:22:42 +0200

    feat: attach sonar drawing to the mod

--- fabric/any/src/main/java/com/midnightbits/scanner/fabric/FabricAnimationHost.java
+++ fabric/any/src/main/java/com/midnightbits/scanner/fabric/FabricAnimationHost.java
@@ -2,9 +2,16 @@ package com.midnightbits.scanner.fabric;
 
 import com.midnightbits.scanner.sonar.Sonar;
 import com.midnightbits.scanner.sonar.graphics.AbstractAnimatorHost;
+import com.midnightbits.scanner.sonar.graphics.GraphicContext;
+import com.midnightbits.scanner.sonar.graphics.Shimmers;
 import com.midnightbits.scanner.utils.Clock;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FabricAnimationHost extends AbstractAnimatorHost {
     public static final FabricAnimationHost INSTANCE = new FabricAnimationHost();
@@ -12,14 +19,30 @@ public class FabricAnimationHost extends AbstractAnimatorHost {
 
     public void initialize(Sonar source) {
         this.source = source;
-        ClientTickEvents.START_WORLD_TICK.register((client) -> {
+        ClientTickEvents.END_CLIENT_TICK.register((client) -> {
             this.tick(Clock.currentTimeMillis());
         });
+        WorldRenderEvents.LAST.register(this::renderLevel);
     }
 
     @Override
     public void tick(long now) {
         super.tick(now);
         this.source.removeOldEchoes();
+    }
+
+    private static final class GatherShimmers implements GraphicContext {
+        List<Shimmers> cloud = new ArrayList<>();
+
+        @Override
+        public void drawScan(List<Shimmers> shimmers) {
+            this.cloud.addAll(shimmers);
+        }
+    };
+
+    private void renderLevel(WorldRenderContext context) {
+        final var shimmers = new GatherShimmers();
+        this.run(shimmers);
+        Pixel.renderLevel(context, source.echoes(), shimmers.cloud);
     }
 }
